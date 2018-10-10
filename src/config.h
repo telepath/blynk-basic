fc36f57affa6095e36f6a5482e51b62dbc892aed
Telepath <cad@dev.telepath.de>
2018-10-10 21:13:12 +0200

    move BlynkProvisioning toits own repo

--- src/config.h
+++ src/config.h
@@ -2,44 +2,36 @@
 #define CONFIG_H
 
 #include <Arduino.h>
+#include <BlynkSimpleEsp8266.h>
 
-#define BOARD_FIRMWARE_VERSION        "1.0.1"
-#define BOARD_HARDWARE_VERSION        "1.0.0"
+#define USE_NODE_MCU_BOARD
+#define SERIAL_DEBUG        //Comment this out to disable Serial outputs
+#define BOARD_LED_BUTTON
 
-#define BOARD_NAME                    "Product Name"        // Name of your product. Should match App Export request info.
-#define BOARD_VENDOR                  "Company Name"        // Name of your company. Should match App Export request info.
-#define BOARD_TEMPLATE_ID             "TMPL0000"
-
-#define PRODUCT_WIFI_SSID             "Our Product"         // Name of the device, to be displayed during configuration. Should match export request info.
-#define BOARD_CONFIG_AP_URL           "our-product.cc"      // Config page will be available in a browser at 'http://our-product.cc/'
+#include "Settings.h"
 
 /* Comment this out to disable prints and save space */
 #ifdef SERIAL_DEBUG
-#define APP_DEBUG        // Comment this out to disable debug prints
-#define BLYNK_PRINT Serial // Defines the object that is used for printing
-// #define BLYNK_DEBUG        // Optional, this enables more detailed prints
+  #define APP_DEBUG        // Comment this out to disable debug prints
+  #define BLYNK_PRINT terminal // Print debug outputs to terminal widget
+  // #define BLYNK_PRINT Serial // Print debug outputs to Serial port
+  // #define BLYNK_DEBUG        // Optional, this enables more detailed prints
+  #define DEBUG(ARG) terminal.print(ARG)
+  #define DEBUGLN(ARG) terminal.println(ARG)
+  // #define DEBUG(ARG) Serial.print(ARG)
+  // #define DEBUGLN(ARG) Serial.println(ARG)
+#else
+  #define DEBUG
+  #define DEBUGLN
 #endif
 
-#define BLYNK_GREEN     "#23C48E"
-#define BLYNK_BLUE      "#04C0F8"
-#define BLYNK_YELLOW    "#ED9D00"
-#define BLYNK_RED       "#D3435C"
-#define BLYNK_DARK_BLUE "#5F7CD8"
-
 enum pins {
-  LED_COLOR,  //vPin 0 - EXAMPLE
-              // add more pins here
-  PIN_NUM     //last item, number of pins
+  TERMINAL,       //vPin 0
+  PIN_NUM         //last item, number of pins
 };
 
-#define USE_NODE_MCU_BOARD
-#define SERIAL_DEBUG
-#define BOARD_LED_BUTTON
-#define DEBUG(ARG) Serial.print(ARG)
-#define DEBUGLN(ARG) Serial.println(ARG)
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-// char auth[] = "";
+WidgetTerminal terminal(TERMINAL);  // Comment this out if you won't use the terminal widget
+
 // digital pins
 
 #endif
